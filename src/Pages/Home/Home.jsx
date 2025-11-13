77064bb96d118a35e3b2e160751642587386c44d
HasinIshrakK <hasinishrakk@gmail.com>
2025-11-13 23:19:35 +0600

    Response interceptor added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -87,6 +87,7 @@ const Home = () => {
                     </motion.div>
                 </section>
             </div>
+            <h2 className="text-3xl font-bold text-center mb-8 mt-20">Recently Added</h2>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-10'>
                 {
                     sortedJobs.slice(0, 6).map(job => <Jobs key={job._id} job={job}></Jobs>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,24 +1,42 @@
 import axios from "axios";
 import useAuth from "./useAuth";
 import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create(
     {
-        baseURL: 'http://localhost:3000',
+        baseURL: import.meta.env.VITE_API_URL,
     },
 );
 
 const useAxiosSecure = () => {
-    const { user } = useAuth();
+    const { user, logout } = useAuth();
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const requestInterceptor = instance.interceptors.request.use(config => {
-            config.headers.Authorization = `Bearer ${user.accessToken}`
+            config.headers.authorization = `Bearer ${user.accessToken}`
             return config;
+        })
+
+        const responseInterceptor = instance.interceptors.response.use(res => {
+            return res;
+        }, err => {
+            const status = err.response?.status;
+            if (status === 401 || status === 403) {
+                logout()
+                    .then(
+                        () => {
+                            navigate('/auth/login')
+                        }
+                    )
+            }
         })
 
         return () => {
             instance.interceptors.request.eject(requestInterceptor)
+            instance.interceptors.response.eject(responseInterceptor)
         }
 
     }, [user]);
